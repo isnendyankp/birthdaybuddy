08c7260ea1e994ff3856ec6af241a2a38e7a28be
isnendyankp <isnendyankp@gmail.com>
2023-07-27 10:37:00 +0700

    s7-169:checking people with log

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { useState } from "react";
 import data from "./data";
 
 const App = () => {
- const [people, setPeople] = useState(data);
+  const [people, setPeople] = useState(data);
+  console.log(people);
   return <h2>Birthday Reminder - Starter</h2>;
 };
 export default App;
@@ -11,4 +12,5 @@ export default App;
 // - import state & data
 // - hook data : useState(data)
 // - add value and function name on hook
+// - checking people with log
 
