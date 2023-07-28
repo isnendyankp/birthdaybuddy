4a81580368f9964392a546bc1bdf30907d8ac3b5
isnendyankp <isnendyankp@gmail.com>
2023-07-28 09:50:19 +0700

    s7-170:Accessing people prop

--- src/List.jsx
+++ src/List.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 
-const List = () => {
-  return (
-    <div>List</div>
-  )
-}
+const List = ({ people }) => {
+  return <div>List</div>;
+};
 
 export default List
 
 // progress:
 // - cr8 base render List
+// - accessing people prop
