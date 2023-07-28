08c47e8793e64e3ed9306b3c1c62671bc2fc2dbb
isnendyankp <isnendyankp@gmail.com>
2023-07-28 09:31:33 +0700

    s7-170:Import list

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import data from "./data";
+import List from './List';
 
 const App = () => {
   const [people, setPeople] = useState(data);
   return <main>
     <section className="container">
       <h3>{people.length} birthday today</h3>
+      <List />
     </section>
   </main>
 };
@@ -18,4 +20,5 @@ export default App;
 // - checking people with log
 // - remove log & base structure
 // - update structure with add section container & pass in people.length
+// - import list 
 
