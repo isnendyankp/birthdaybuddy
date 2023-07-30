2baddc18c55c82dc06f3f6667b00b9dbd84cc14a
isnendyankp <isnendyankp@gmail.com>
2023-07-30 14:18:03 +0700

    s7-171:Add 3 parameter for render person

--- src/Person.jsx
+++ src/Person.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 
-const Person = () => {
-  return (
-    <div>Person</div>
-  )
-}
+const Person = ({ image, name, age }) => {
+  return <div>Person</div>;
+};
 
 export default Person
 
 // progress:
 // - cr8 base Person
+// - add 3 parameter for render person
